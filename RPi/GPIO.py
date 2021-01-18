c08a38fd60aa5acbb9378fe326c0daee5364a377
Adi Miller <adimiller@gmail.com>
2021-01-18 20:50:22 +0200

    Add GPIO mocks

---
+++ RPi/GPIO.py
@@ -0,0 +1,20 @@
+BOARD = 1
+OUT = 1
+IN = 1
+BCM = 1
+
+def setmode(a):
+   pass
+  
+def setup(a, b):
+   pass
+
+def output(a, b):
+   pass
+  
+def cleanup():
+   pass
+  
+def setwarnings(flag):
+   pass
+  
