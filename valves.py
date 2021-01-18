95cb2b6bce10410f7c14140044929f66811d3daf
Adi Miller <adimiller@gmail.com>
2021-01-18 18:23:20 +0200

    Initial support for GPIO

--- valves.py
+++ valves.py
@@ -1,8 +1,10 @@
 import time
+import RPi.GPIO as GPIO
 
 class BaseValve():
-  def __init__(self, logger):
+  def __init__(self, logger, config):
     self.logger = logger
+    self.config = config
 
   def open(self):
     self.logger.info("Opening valve")
@@ -20,20 +22,32 @@ class TestValve(BaseValve):
     time.sleep(0.5)
 
 class ThreeWireValve(BaseValve):
-  def __init__(self, logger):
-    BaseValve.__init__(self, logger)
+  def __init__(self, logger, config):
+    BaseValve.__init__(self, logger, config)
+    self.gpioOn = config['gpioOn']
+    self.gpioOff = config['gpioOff']
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setwarnings(False)
+    GPIO.setup(self.gpioOn, GPIO.OUT)
+    GPIO.setup(self.gpioOff, GPIO.OUT)
 
   def open(self):
     BaseValve.open(self)
+    GPIO.output(self.gpioOn, GPIO.HIGH)
+    time.sleep(0.5)
+    GPIO.output(self.gpioOn, GPIO.LOW)
 
   def close(self):
     BaseValve.close(self)
+    GPIO.output(self.gpioOff, GPIO.HIGH)
+    time.sleep(0.5)
+    GPIO.output(self.gpioOff, GPIO.LOW)
 
 def valveFactory(type, logger, config):
   if type == 'test':
-    return TestValve(logger)
+    return TestValve(logger, config)
 
   if type == '3wire':
-    return ThreeWireValve(logger)
+    return ThreeWireValve(logger, config)
 
   raise Exception("Cannot find implementation for valve type '%s'." % type)
